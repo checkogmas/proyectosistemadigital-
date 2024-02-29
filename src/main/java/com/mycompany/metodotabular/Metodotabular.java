4e49b240ff911f8ba3760597812055f4f2f8bbbb
Dell <Dell@192.168.0.134>
2024-02-29 09:23:15 -0600

    29/02/2024 pryecto digital primera actualizacion

---
+++ src/main/java/com/mycompany/metodotabular/Metodotabular.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.metodotabular;
+
+/**
+ *
+ * @author Dell
+ */
+public class Metodotabular {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
